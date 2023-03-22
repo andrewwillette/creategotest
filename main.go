51cf042de076c7566941718baf15d2c98a0d7a40
Andrew Willette <willette.andrew1846@gmail.com>
2023-03-22 14:44:15 -0500

    master: TestTemplate struct with methods POC implementation

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"regexp"
 	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 func main() {
@@ -17,29 +20,6 @@ func main() {
 	fmt.Printf("%v\n", string(data))
 }
 
-const testTemplate = `
-func Test<functionNameTest>(t *testing.T) {
-	var cases = []struct {
-		<caseValues>
-	}{
-		{
-		},
-	}
-	for _, c := range cases {
-		t.Run(fmt.Sprintf("%v", c.input), func(t *testing.T) {
-			result := <functionName>()
-			require.Equal(t, c.expected, result)
-		})
-	}
-}
-`
-
-func getFuncName(file []byte) string {
-	r := regexp.MustCompile(`func \w*`)
-	funcWithSpace := strings.TrimLeft(r.FindStringSubmatch(string(file))[0], "func")
-	return strings.TrimLeft(funcWithSpace, " ")
-}
-
 func getTestFile() string {
 	dirname, err := os.UserHomeDir()
 	if err != nil {
@@ -48,6 +28,118 @@ func getTestFile() string {
 	return dirname + "/git/leetcode-practice/go-leetcode/canplaceflowers/canplaceflowers_test.go"
 }
 
-func getTestFunctionAsString(funcName string, returnType string, funcParams map[string]string) string {
-	return ""
+var (
+	testTemplate = `
+func Test<testFunctionSplint>(t *testing.T) {
+	var cases = []struct {
+		<caseSplint>
+		<expectedSplint>
+	}{
+		{
+		},
+	}
+	for _, c := range cases {
+		t.Run(fmt.Sprintf(""), func(t *testing.T) {
+			result := <functionSplint>(<paramsSplint>)
+			require.Equal(t, c.expected, result)
+		})
+	}
+}
+`
+)
+
+const (
+	testFuncSplint = "<testFunctionSplint>"
+	funcSplint     = "<functionSplint>"
+	paramsSplint   = "<paramsSplint>"
+	caseSplint     = "<caseSplint>"
+	expectedSplint = "<expectedSplint>"
+)
+
+type FuncToTest struct {
+	function string
+}
+
+func (funcToTest *FuncToTest) getFuncName() string {
+	r := regexp.MustCompile(`func \w*`)
+	funcWithSpace := strings.TrimLeft(r.FindStringSubmatch(funcToTest.function)[0], "func")
+	return strings.TrimLeft(funcWithSpace, " ")
+}
+
+func (funcToTest *FuncToTest) getFuncParams() map[string]string {
+	funcParams := make(map[string]string)
+	return funcParams
+}
+
+func (funcToTest *FuncToTest) getReturnType() string {
+	r := regexp.MustCompile(`\) \w* {`)
+	returnVal := r.FindStringSubmatch(funcToTest.function)[0]
+	returnVal = strings.TrimLeft(returnVal, ") ")
+	returnVal = strings.TrimRight(returnVal, " {")
+	return returnVal
+}
+
+type TestTemplate struct {
+	template string
+}
+
+func (template *TestTemplate) getTestFunctionAsString(funcName string, returnType string, funcParams map[string]string) string {
+	template.insertTestFunctionSplint(funcName)
+	template.insertFunctionSplint(funcName)
+	template.insertParamsSplint(funcParams)
+	template.insertCaseSplint(funcParams)
+	template.insertExpectedSplint(returnType)
+	return template.template
+}
+
+func (template *TestTemplate) insertTestFunctionSplint(funcName string) {
+	m := regexp.MustCompile(testFuncSplint)
+	res := m.ReplaceAllString(template.template, cases.Title(language.English, cases.NoLower).String(funcName))
+	template.template = res
+}
+
+func (template *TestTemplate) insertFunctionSplint(funcName string) {
+	m := regexp.MustCompile(funcSplint)
+	res := m.ReplaceAllString(template.template, funcName)
+	template.template = res
+}
+
+func (template *TestTemplate) insertCaseSplint(funcParams map[string]string) {
+	var sb strings.Builder
+	cnt := 0
+	for k, v := range funcParams {
+		if cnt < len(funcParams)-1 {
+			sb.WriteString(k + " " + v + "\n")
+		} else {
+			sb.WriteString(k + " " + v)
+		}
+		cnt++
+	}
+	m := regexp.MustCompile(caseSplint)
+	res := m.ReplaceAllString(template.template, sb.String())
+	template.template = res
+}
+
+func (template *TestTemplate) insertExpectedSplint(returnType string) {
+	m := regexp.MustCompile(expectedSplint)
+	var sb strings.Builder
+	sb.WriteString("expected " + returnType)
+	res := m.ReplaceAllString(template.template, sb.String())
+	template.template = res
+}
+
+func (template *TestTemplate) insertParamsSplint(params map[string]string) {
+	m := regexp.MustCompile(paramsSplint)
+	var sb strings.Builder
+	cnt := 0
+	for k := range params {
+		if cnt < len(params)-1 {
+			sb.WriteString("c." + k + ", ")
+		} else {
+			sb.WriteString("c." + k)
+		}
+		cnt++
+	}
+	res := m.ReplaceAllString(template.template, sb.String())
+	template.template = res
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,14 +15,69 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
 }
 `
 
+var (
+	template   = TestTemplate{template: testTemplate}
+	funcToTest = FuncToTest{function: exampleGoFile}
+)
+
 func TestGetFuncName(t *testing.T) {
-	funcName := getFuncName([]byte(exampleGoFile))
+	funcToTest := FuncToTest{function: exampleGoFile}
+	funcName := funcToTest.getFuncName()
 	require.Equal(t, "canPlaceFlowers", funcName)
+}
+
+func TestGetReturnType(t *testing.T) {
+	funcToTest := FuncToTest{function: exampleGoFile}
+	returnType := funcToTest.getReturnType()
+	require.Equal(t, "bool", returnType)
 }
 
 func TestGetTestFile(t *testing.T) {
 	getTestFile()
 }
 
-func TestGetTestString(t *testing.T) {
+func TestGetTestFunctionAsString(t *testing.T) {
+	funcParams := map[string]string{
+		"flowerbed": "[]int",
+		"n":         "int",
+	}
+	result := template.getTestFunctionAsString("canPlaceFlowers", "bool", funcParams)
+	require.Contains(t, result, "TestCanPlaceFlowers(t *testing.T)")
+	require.Contains(t, template.template, "canPlaceFlowers(")
+	require.Contains(t, template.template, "flowerbed []int\nn int")
+	require.Contains(t, template.template, "expected bool")
+	require.Contains(t, template.template, "(c.flowerbed, c.n)")
+}
+
+func TestInsertTestFunctionSplint(t *testing.T) {
+	template.insertTestFunctionSplint("canPlaceFlowers")
+	require.Contains(t, template.template, "TestCanPlaceFlowers(t *testing.T)")
+}
+
+func TestInsertFunctionSplint(t *testing.T) {
+	template.insertFunctionSplint("canPlaceFlowers")
+	require.Contains(t, template.template, "canPlaceFlowers(")
+}
+
+func TestInsertCaseSplint(t *testing.T) {
+	funcParams := map[string]string{
+		"flowerbed": "[]int",
+		"n":         "int",
+	}
+	template.insertCaseSplint(funcParams)
+	require.Contains(t, template.template, "flowerbed []int\nn int")
+}
+
+func TestInsertExpectedSplint(t *testing.T) {
+	template.insertExpectedSplint("int")
+	require.Contains(t, template.template, "expected int")
+}
+
+func TestInsertParamsSplint(t *testing.T) {
+	funcParams := map[string]string{
+		"flowerbed": "[]int",
+		"n":         "int",
+	}
+	template.insertParamsSplint(funcParams)
+	require.Contains(t, template.template, "(c.flowerbed, c.n)")
 }
